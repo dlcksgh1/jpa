b11c0995c34dec3630caa68241de6a40a123884c
Lee  Chanho <sma05091@nate.com>
2023-07-26 22:32:16 +0900

    회원 등록 • 회원 수정 • 회원 삭제 • 회원 단 건 조회 • JPQL로 전체 회원 검색

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -16,11 +17,39 @@ public class JpaMain {
         tx.begin();
 
         try{
+            /*      
+            생성
             Member member = new Member();
             member.setId(1L);
             member.setName("HelloA");
 
             em.persist(member);
+            */
+
+            /*      
+            조회
+            Member findMember = em.find(Member.class, 1L);
+            System.out.println("findMember.id" + findMember.getId());
+            System.out.println("findMember.name" + findMember.getName());
+
+            */
+
+            /*
+            수정
+            Member findMember = em.find(Member.class, 1L);
+            findMember.setName("helloJPA");
+            
+            */
+
+            // JPQL
+            List<Member> resultList = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(1)
+                    .setMaxResults(10)
+                    .getResultList();
+
+            for (Member member : resultList) {
+                System.out.println("member.Name = " + member.getName());
+            }
 
             tx.commit();
         }catch (Exception e){
