0bf1a80c538c60dd196488ab617d26611540de2b
Lee  Chanho <sma05091@nate.com>
2023-07-28 21:59:19 +0900

    기본키 매핑 IDENTITY,  SEQUENCE

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -6,9 +6,21 @@ import java.time.LocalDateTime;
 import java.util.Date;
 
 @Entity
+@SequenceGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        sequenceName = "MEMBER_SEQ",  //매핑할 데이터베이스 시퀀스 이름
+        initialValue = 1, allocationSize = 50)
 public class Member {
 
-    @Id
+/*
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;*/
+
+    @Id @GeneratedValue(strategy = GenerationType.SEQUENCE,
+            generator = "MEMBER_SEQ_GENERATOR")
     private Long id;
 
     @Column(name = "name")
