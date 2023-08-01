3e788cbc56437ac888a87c3a775591a524872f27
Lee  Chanho <sma05091@nate.com>
2023-08-01 21:36:22 +0900

    JPA 다양한 연관관계 매핑 - 일대다 단방향 양방향

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -28,7 +28,7 @@ public class Member {
     private String username;
 
     @ManyToOne
-    @JoinColumn(name = "TEAM_ID")
+    @JoinColumn(name = "TEAM_ID" ,insertable=false, updatable=false)
     private Team team;
 
     private Integer age;
